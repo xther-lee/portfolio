f1233c3b3b4ffdc11d6183b00f13fad58f842f8e
당신의이름 <1205est@naver.com>
2025-09-08 11:50:08 +0900

    파비콘 수정 및 링크 페딩 주기

--- src/Bar/Header.jsx
+++ src/Bar/Header.jsx
@@ -7,21 +7,21 @@ const Header = () => {
         <div className="nav">
             <nav className="navbar">
                 <div className="nav-container">
-                <div className="nav-logo">
-                    <a href="https://xther-lee.github.io/portfolio/"  className='logo-text'>이에스더 | 프론트엔드</a>
-                </div>
-                <div className='nav-menu'>
-                    <Link to='/project'>
-                        Project
-                    </Link>
-                    <Link to='/about'>
-                        About me
-                    </Link>
-                    <a href="https://github.com/xther-lee" 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                    className='git'>Github</a>
-                </div>
+                    <div className="nav-logo">
+                        <a href="https://xther-lee.github.io/portfolio/"  className='logo-text'>이에스더 | 프론트엔드</a>
+                    </div>
+                    <div className='nav-menu'>
+                        <Link to='/project'>
+                            Project
+                        </Link>
+                        <Link to='/about'>
+                            About me
+                        </Link>
+                        <a href="https://github.com/xther-lee" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className='git'>Github</a>
+                    </div>
                 </div>
             </nav>
         </div>
